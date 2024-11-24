fab52656ea936683c41f61a6e6da92a7c6aa0e8e
Frederik Boysen <freboysen@gmail.com>
2024-11-25 00:00:42 +0100

    setting reference API project

--- BookTradingHub.WebAPI/Controllers/BooksController.cs
+++ BookTradingHub.WebAPI/Controllers/BooksController.cs
@@ -1,7 +1,7 @@
-using BookTradingHub.Database.Data;
-using BookTradingHub.WebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using BookTradingHub.Database.Data; // Namespace for ApplicationDB context
+using BookTradingHub.WebAPI.Models; // Namespace for Book model
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -13,16 +13,19 @@ namespace BookTradingHub.WebAPI.Controllers
     {
         private readonly ApplicationDB _context;
 
+        // Constructor to inject the DbContext
         public BooksController(ApplicationDB context)
         {
             _context = context;
         }
 
-        // GET: api/Books
+        // GET: api/books (this is the endpoint Blazor calls)
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        public async Task<ActionResult<List<Book>>> GetBooks()
         {
-            return await _context.Books.ToListAsync();
+            // The query to fetch all books from the Books table
+            var books = await _context.Books.ToListAsync(); // This is the equivalent of "SELECT * FROM Books"
+            return Ok(books); // Return the books as JSON response
         }
     }
 }
--- BookTradingHub.WebApp/Program.cs
+++ BookTradingHub.WebApp/Program.cs
@@ -1,26 +1,30 @@
 using BookTradingHub.WebApp.Components;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add HttpClient with the base URL of your API
+builder.Services.AddHttpClient("WebAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5001/");  // Your WebAPI base URL
+});
+
+// Add Razor Components
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
