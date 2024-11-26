1ced82aece69dac6adcf9a71a827aa44d2344488
Jonas Skjøth <jonas.84@live.dk>
2024-11-26 19:35:04 +0100

    Created top navbar and inserted logo

--- BookTradingHub.WebAPI/obj/Debug/net9.0/BookTradingHub.WebAPI.AssemblyInfo.cs
+++ BookTradingHub.WebAPI/obj/Debug/net9.0/BookTradingHub.WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookTradingHub.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83747320f12bc1d60be39f7b6207e852ed67c60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab52656ea936683c41f61a6e6da92a7c6aa0e8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookTradingHub.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookTradingHub.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
